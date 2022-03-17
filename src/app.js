727883ebb23d1183596632d96fdec396f47e1cf5
Watermana <watermanja9@gmail.com>
2022-03-17 12:23:35 -0600

    added migrations

--- src/app.js
+++ src/app.js
@@ -1,5 +1,26 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+
+const moviesRouter = require("./movies/movies.router");
+
+
 const app = express();
+
+app.use(cors());
+
+app.use("/movies", moviesRouter)
+
+// Not found handler
+app.use((req, res, next) => {
+    next({ status: 404, message: `Not found: ${req.originalUrl}` });
+});
+
+// Error handler
+app.use((error, req, res, next) => {
+    console.error(error);
+    const { status = 500, message = "Something went wrong!" } = error;
+    res.status(status).json({ error: message });
+});
 
 module.exports = app;
---
+++ src/movies/movies.controller.js
@@ -0,0 +1,12 @@
+const req = require("express/lib/request");
+const moviesService = require("./movies.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+
+async function list(req, res, next) {
+    const data = await moviesService.list();
+    res.json({data});
+}
+
+module.exports = {
+    list: asyncErrorBoundary(list),
+};
